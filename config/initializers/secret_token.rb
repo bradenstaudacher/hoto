a793e2b509bf7417709f0ac252d3ef17fdad5a7c
Ben <benctemple@gmail.com>
2015-04-11 15:12:40 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hoto::Application.config.secret_key_base = '7a96f8c16adb599f5a2c023fe42139f65f56d606331d3261cef35f7ed7da506d414aea8216463f82df50db55323bef41d39deecffecef55c71cc42032acfa063'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hoto::Application.config.session_store :cookie_store, key: '_hoto_session'
