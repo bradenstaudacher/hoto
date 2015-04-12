67e1c479eceade82dc48ccf1fdca821bf5b924c6
JulianOrinyol <julianorinyol@gmail.com>
2015-04-11 21:48:50 -0700

    game board is now retrieving square height from db

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,17 +2,41 @@ class Game < ActiveRecord::Base
   has_many :squares
   has_and_belongs_to_many :players
   
-  BOARD = [
-    ['x','x','x','x','x'],
-    ['x','x','x','x','x'],
-    ['x','x','x','x','x'],
-    ['x','x','x','x','x'],
-    ['x','x','x','x','x']
-  ] 
+  # BOARD = [
+  #   ['x','x','x','x','x'],
+  #   ['x','x','x','x','x'],
+  #   ['x','x','x','x','x'],
+  #   ['x','x','x','x','x'],
+  #   ['x','x','x','x','x']
+  # ] 
 
- def board
-  Square.all.each do |square|
-  
- end
 
+  class << self 
+
+    def board
+      board = []
+      row1 = []
+      row2 = []
+      row3 = []
+      row4 = []
+      row5 = []
+      Square.all.each do |square, index|
+        if row1.length < 5
+          row1 << square.height
+        elsif row2.length < 5
+          row2 << square.height
+        elsif row3.length < 5
+          row3 << square.height
+        elsif row4.length < 5
+          row4 << square.height
+        else 
+          row5 << square.height
+        end
+      end
+      board << row1 << row2 << row3 << row4 << row5
+    end
+
+    # BOARD = board
+
+  end
 end
