6eaed7fe096eed787d141f7d9f171397d5823729
bradenstaudacher <braden.staudacher@gmail.com>
2015-04-15 01:27:02 +0000

    started the process

--- spec/models/square_spec.rb
+++ spec/models/square_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Square, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    counter = 1
+    while counter <= 5 
+      i = 1
+      while i <= 5
+        Square.create(x: counter, y: i, height: 0)
+        i += 1
+      end
+      counter += 1
+    end
+  end
+
+  it "creates the squares" do
+    expect(Square.all.length).to eq(25)
+  end
 end
