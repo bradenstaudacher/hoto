26bee8fd2e844df6f6d34668297cd1b916c9b962
Ben <benctemple@gmail.com>
2015-04-16 20:36:22 -0700

    users can now delete their own accounts, but not others

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
+  # GET /users/1 
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
@@ -64,9 +64,12 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
+
     @user.destroy
+    session[:user_id] = nil
     respond_to do |format|
-      format.html { redirect_to users_url }
+      format.html { redirect_to games_path }
       format.json { head :no_content }
     end
   end
