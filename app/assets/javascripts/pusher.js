927bd76a89937673a0452df4f7f24203e8e673d5
JulianOrinyol <julianorinyol@gmail.com>
2015-04-24 21:29:57 -0700

    topple animation is working to the right side

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -25,25 +25,91 @@ $(document).ready(function(){
    currentPhase = phase;
 
 
-
    if (game === parseInt(currentGame)) {
     if (!gameActive){
        $('#info-div').append('<p>'+ winnerName +' won the hoto</p>');
     } 
+    // for(var i=0; i < 25; i++){
+    //   console.log(i);
+    //   $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').removeClass('black white empty');
+    //   $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').addClass(squares[i].colour);
+    //   $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').html("<span class='pieces'>"+ squares[i].height + "</span>");
+    // }
+    $('#turn-phase').text("It is " + turnstate + "'s turn to " + phase);
 
-     for(var i=0; i < 25; i++){
-        $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').removeClass('black white empty');
-        $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').addClass(squares[i].colour);
-        $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"] .pieces').text(squares[i].height);
-      }
-        $('#turn-phase').text("It is " + turnstate + "'s turn to " + phase);
-    }
-  });
+    if (turn) {
+      for(var i = 0; i < turn.actions.length; i ++){
+
+
+        if (data.turn.actions[i].action_type === 'place') {
+          console.log("in pusher place")
+          var square_coords = data.turn.actions[0].coord
+            // for(var i=0; i < 25; i++){
+            //     console.log(i);
+            //     $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').removeClass('black white empty');
+            //     $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').addClass(squares[i].colour);
+            //     $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').html("<span class='pieces'>"+ squares[i].height + "</span>");
+            // }
+          changeGameBoard(squares);
+        }else if (data.turn.actions[i].action_type === 'topple'){
+          console.log("in pusher topple ")
+          var square_coords = data.turn.actions[0].coord
+           $('td.game-square[data-x="'+ data.turn.actions[0].coord.x+'"][data-y="'+ data.turn.actions[0].coord.y +'"] span:nth-child(1)').animate({ 
+          left: "+=130"
+          }, 1000)
+           $('td.game-square[data-x="'+ data.turn.actions[0].coord.x+'"][data-y="'+ data.turn.actions[0].coord.y +'"] span:nth-child(2)').animate({ 
+          left: "+=235"
+          }, 1000)
+          $('td.game-square[data-x="'+ data.turn.actions[0].coord.x+'"][data-y="'+ data.turn.actions[0].coord.y +'"] span:nth-child(3)').animate({ 
+          left: "+=360"
+          }, 1000)
+          setTimeout(function(){
+            changeGameBoard(squares)
+            
+          }, 2000)
+        }else {
+          console.log("in pusher bloom")
+          var square_coords = data.turn.actions[0].coord
+
+          // setTimeout(function(){
+          //   changeGameBoard();
+            
+          // }, 3000);
+        }
     
 
+      }
+    }
+    
+
+    }
+  function changeGameBoard(squares){  
+     for(var i=0; i < 25; i++){
+      $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').removeClass('black white empty');
+      $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').addClass(squares[i].colour);
+      $('td.game-square[data-x="'+squares[i].x+'"][data-y="'+squares[i].y+'"]').html(createPieces(squares[i].height));
+    }
+  }
+
+
+  function createPieces(number){
+    x = "<span class='pieces'>" + number + "</span>"
+    return Array(number + 1).join(x)
+  }
+
+  }); //channel.bind
+    
   // channel.bind('pusher:subscription_succeeded', function(data) {
   //  console.log('pusher subscription succeeded', data.test);
   // });
 
 
 });//end document ready
+
+
+
+// $('.pieces').on('click',function(){
+//   $(this).animate({ 
+//      left: "+=50"
+//   }, 1000)
+// })
