363d9306d544908f25b2a8f9af08f64fd8d2209f
bradenstaudacher <braden.staudacher@gmail.com>
2015-04-25 04:16:50 +0000

    changed seed file to have more games

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: "bigdog", games_played: 0, games_won: 0, email: "big@dog.com", current_rating: 0, password: "testes", password_confirmation: "testes")
-User.create(name: "crunchy pete", games_played: 0, games_won: 0, email: "cool@dog.com", current_rating: 0, password: "testes", password_confirmation: "testes")
+
+########################### USERS ###########################
+User.create(name: "bigdog", games_played: 2, games_won: 1, email: "big@dog.com", current_rating: 1244, password: "testes", password_confirmation: "testes")
+User.create(name: "crunchy pete", games_played: 32, games_won: 12, email: "cool@dog.com", current_rating: 1067, password: "testes", password_confirmation: "testes")
+User.create(name: "leadboy", games_played: 5, games_won: 3, email: "lead@dog.com", current_rating: 907, password: "testes", password_confirmation: "testes")
+User.create(name: "curliyq", games_played: 90, games_won: 80, email: "curly@dog.com", current_rating: 2011, password: "testes", password_confirmation: "testes")
+User.create(name: "froggod77", games_played: 12, games_won: 5, email: "frog@dog.com", current_rating: 989, password: "testes", password_confirmation: "testes")
+User.create(name: "gamorhod", games_played: 4, games_won: 1, email: "gamo@dog.com", current_rating: 1557, password: "testes", password_confirmation: "testes")
+User.create(name: "lodki", games_played: 1, games_won: 0, email: "lmanswan@dog.com", current_rating: 823, password: "testes", password_confirmation: "testes")
+User.create(name: "codman", games_played: 3, games_won: 0, email: "codman@dog.com", current_rating: 1450, password: "testes", password_confirmation: "testes")
+
+########################### GAMES TO SPECTATE ###########################
 
 game_id_counter = 1
-
-7.times do 
+user_counter = 1
+5.times do 
   
   Game.create(turnstate: "white", active: true, phase: 'place', moves_counter: 0)
-
   counter = 1
 
   while counter <= 5
@@ -26,16 +35,41 @@ game_id_counter = 1
     end
     counter += 1
   end
-  Game.find(game_id_counter).users << User.first
-  p = GamesUser.where(game_id: game_id_counter).where(user_id: User.first.id)[0]
+  Game.find(game_id_counter).users << User.find(user_counter)
+  p = GamesUser.where(game_id: game_id_counter).where(user_id: User.find(user_counter).id)[0]
   p.colour = 'white'
   p.save
-  Game.find(game_id_counter).users << User.last
-  n = GamesUser.where(game_id: game_id_counter).where(user_id: User.last.id)[0]
+  Game.find(game_id_counter).users << User.find(user_counter+1)
+  n = GamesUser.where(game_id: game_id_counter).where(user_id: User.find(user_counter+1).id)[0]
   n.colour = 'black'
   n.save
   game_id_counter += 1
+  user_counter += 1
 end
 
+########################### GAMES TO JOIN ###########################
 
+game_id_counter2 = 6
+user_counter2 = 3
+5.times do 
+  
+  Game.create(turnstate: "white", active: true, phase: 'place', moves_counter: 0)
+  counter = 1
+
+  while counter <= 5
+    i = 1
+    while i <= 5
+      puts "i done did make a square!!"
+      Square.create(x: i, y: counter, height: 0, game_id: game_id_counter2, colour: 'empty')
+      i += 1
+    end
+    counter += 1
+  end
+  Game.find(game_id_counter2).users << User.find(user_counter2)
+  p = GamesUser.where(game_id: game_id_counter2).where(user_id: User.find(user_counter2).id)[0]
+  p.colour = 'white'
+  p.save
+  game_id_counter2 += 1
+  user_counter2 += 1
+end
 
