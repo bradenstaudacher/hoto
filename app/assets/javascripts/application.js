9a372e4269863a5a47110ace4dc38fff3072d9f3
bradenstaudacher <braden.staudacher@gmail.com>
2015-04-26 19:44:37 +0000

    added greyed out resign button when your turn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -119,6 +119,7 @@ console.log('inside doTheGame in application.js')
 
 
 
+
             },
             error: function(hash, message){
               console.log(message);
@@ -149,6 +150,12 @@ console.log('inside doTheGame in application.js')
 
       }
     });
+
+    if ((currentUser !== 0) && (currentTurnstate !== currentUserColour)) {
+      $('#resign-btn').addClass('disabled');
+    }
+
+
      
 }
 
