aa47793cc6e53dc8ce8e37a461dcd6a3316e9855
JulianOrinyol <julianorinyol@gmail.com>
2015-04-30 16:36:21 -0700

    Braden making this change: get's rid of an additional minus and has chart order correctified

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,25 +8,24 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
-  def recent_rating_change
-    total_change = 0
-    User.find(id).elo_changes.limit(5).each do |elo_change|
-      total_change += elo_change.change
-    end
-    total_change > 0 ? total_change = "+" + total_change.to_s : total_change = "-" + total_change.to_s
-    return total_change
-  end
+ def recent_rating_change
+   total_change = 0
+   User.find(id).elo_changes.last(5).each do |elo_change|
+     total_change += elo_change.change
+   end
+   total_change > 0 ? total_change = "+" + total_change.to_s : total_change = total_change.to_s
+   return total_change
+ end
 
-  def chart_rating_change
-    rating = 0
-    user = User.find(id)
-    chart_change = []
-    user.elo_changes.limit(10).each do |elo_change|
-      rating = elo_change.change
-      chart_change << rating
-    end
-    return chart_change.reverse
-  end
-
+ def chart_rating_change
+   rating = 0
+   user = User.find(id)
+   chart_change = []
+   user.elo_changes.limit(10).each do |elo_change|
+     rating += elo_change.change
+     chart_change << rating
+   end
+   return chart_change
+ end
 
 end
